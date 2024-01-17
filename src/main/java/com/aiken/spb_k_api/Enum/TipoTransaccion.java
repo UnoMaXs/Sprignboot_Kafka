d3d8a24a14bfde4959c39a9e4ef4f5b3397fa848
Aiken Rojas <pieraiken202@hotmail.com>
2024-01-17 16:52:55 -0500

    Adding Kafka Config, Listener, Consumer

--- src/main/java/com/aiken/spb_k_api/Enum/TipoTransaccion.java
+++
@@ -1,6 +0,0 @@
-package com.aiken.spb_k_api.Enum;
-
-public enum TipoTransaccion {
-    ENTRADA,
-    SALIDA
-}
--- src/main/java/com/aiken/spb_k_api/SpbKApiApplication.java
+++ src/main/java/com/aiken/spb_k_api/SpbKApiApplication.java
@@ -1,7 +1,10 @@
 package com.aiken.spb_k_api;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class SpbKApiApplication {
@@ -10,4 +13,12 @@ public class SpbKApiApplication {
         SpringApplication.run(SpbKApiApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate){
+        return args -> {
+            for (int i = 0; i < 10; i++) {
+                kafkaTemplate.send(/*TOPIC*/"amigoscode",/*MENSAJE*/"Hello kafka c:" + i);
+            }
+        };
+    }
 }
